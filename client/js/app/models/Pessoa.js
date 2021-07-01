e6d2fbd663d2121e90f2d9488d2b19356a79756b
Bruno Anguita <brunoanguita@live.com>
2021-07-01 11:32:30 -0300

    Commit Inicial

---
+++ client/js/app/models/Pessoa.js
@@ -0,0 +1,13 @@
+class Pessoa {
+
+    constructor(nome, sobrenome) {
+
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    obtemNomeCompleto(){
+        return this.nome + ' ' + this.sobrenome;
+    }
+
+}
